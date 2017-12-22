0c6cb24bc6534d8beb63c480faffb547eb3a74e7
Raghavendra Thavva <raghavendrareddy.thavva@usfoods.com>
2017-12-22 10:27:45 -0500

    Sample commit

---
+++ KonySampleAF/studioactions/mobile/Action0eac8b2ce62e648.js
@@ -0,0 +1,7 @@
+function Action0eac8b2ce62e648() {
+    return AS_NamedActions_dc33829432cc490bba0ae1ab4da24915();
+}
+
+function AS_NamedActions_dc33829432cc490bba0ae1ab4da24915() {
+    kony.print("test");
+}
